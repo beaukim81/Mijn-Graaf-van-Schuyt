e6ea401c8f9423ab45324a65ae3a8cfa685fad56
Kim <mailz4kim@gmail.com>
2026-06-01 22:36:39 +0200

    Make web app installable as PWA

--- src/main.tsx
+++ src/main.tsx
@@ -8,3 +8,11 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     <App />
   </React.StrictMode>,
 );
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("/service-worker.js").catch(() => {
+      // Installatie blijft bruikbaar als service worker registratie niet lukt.
+    });
+  });
+}
